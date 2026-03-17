080f7aa398f639db12276619bf1c2ec9d5c805f5
unknown <you@example.com>
2026-03-16 22:08:14 -0500

    Correccion IP

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.3.0/24"
   availability_zone       = "us-east-2b"
   map_public_ip_on_launch = true
 
