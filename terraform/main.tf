6914f7619ab57f0060f1c0ed1fcee24b13932363
unknown <you@example.com>
2026-03-20 11:28:39 -0500

    eliminar

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,14 +66,6 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-}
 
 # ------------------------
 # Route Table Associations
@@ -86,11 +78,6 @@ resource "aws_route_table_association" "public_assoc" {
 resource "aws_route_table_association" "public_2_assoc" {
   subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "private_assoc" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private_rt.id
 }
 
 
