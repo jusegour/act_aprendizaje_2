54a3f53e86d14d5585373fd1fa3b53cfb1f59ffa
unknown <you@example.com>
2026-03-20 11:09:35 -0500

    Elmiminar recurso innecesario

--- terraform/main.tf
+++ terraform/main.tf
@@ -395,24 +395,6 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-resource "aws_acm_certificate" "cert" {
-  domain_name       = "juangomez.com"
-  validation_method = "DNS"
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.cert.arn
-
-  default_action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
-  }
-}
-
 resource "aws_lb_listener_rule" "api_rule" {
   listener_arn = aws_lb_listener.http.arn
 
