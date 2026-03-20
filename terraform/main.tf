eab9b86cde75f92707bab44d36b2a2228fd34d21
unknown <you@example.com>
2026-03-20 10:07:39 -0500

    JGO - Correccion en load balancers

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,6 +26,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidr
+  availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -93,14 +94,6 @@ resource "aws_route_table" "private_rt" {
   }
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "public-rt"
-  }
-}
-
 # ------------------------
 # Route Table Associations
 # ------------------------
@@ -111,7 +104,7 @@ resource "aws_route_table_association" "public_assoc" {
 
 resource "aws_route_table_association" "public_2_assoc" {
   subnet_id      = aws_subnet.public_2.id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table_association" "private_assoc" {
@@ -121,7 +114,7 @@ resource "aws_route_table_association" "private_assoc" {
 
 
 resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
+  route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
